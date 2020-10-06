1d79bba46fb9a62fca51c218ff0f6bb009bf9655
nwe ni naing <nweninaing896@gmail.com>
2020-10-06 10:37:48 -0400

    FBclone create

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get '', to: 'sessions#new'
+  
+  resources :users, only: [:new, :create, :show]
+
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+
+  resources :feeds do
+    collection do
+      post :confirm
+    end
+  end
+end
