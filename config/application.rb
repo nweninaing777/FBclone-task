0429660ec7a79f7a723c6d4b4c613574b28c1cd5
nwe ni naing <nweninaing896@gmail.com>
2020-10-07 01:09:10 -0400

    FBclone create

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,9 @@
 require_relative 'boot'
 
 require 'rails/all'
-
-
 Bundler.require(*Rails.groups)
-
 module Facebook
   class Application < Rails::Application
     config.load_defaults 6.0
-
-
-  end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   get '', to: 'sessions#new'
-  
+
   resources :users, only: [:new, :create, :show]
-
-
   resources :sessions, only: [:new, :create, :destroy]
-
-
   resources :feeds do
     collection do
       post :confirm
